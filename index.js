3cf70d3f2f1b427da7c5ec2d4d260eb257cb5fbc
ManishMaster <manish.rsauto@gmail.com>
2024-04-09 11:52:19 +0530

    add modbus serial

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json());
 
+const ModbusRTU = require("modbus-serial");
+const client = new ModbusRTU();
+const plcIP = "192.168.0.137";
+const plcPort = 502;
+const startAddress = 60;
+const quantity = 2; // Number of holding registers to read
+
 app.get("/", (req, res) => {
-  res.status(200).send("Hello from the server");
+  //   res.status(200).send("Hello from the server");
+
+  client
+    .connectTCP(plcIP, { port: plcPort })
+    .then(async () => {
+      // console.log('Connected to PLC');
+
+      // Read holding registers
+      const data = await client.readHoldingRegisters(startAddress, quantity);
+      // console.log('Holding Registers:', data.data); // Data will be an array containing the values of holding registers
+      res.status(200).json({ data: data.data });
+    })
+    .catch((error) => {
+      // console.error('Error connecting to PLC:', error);
+      res.status(500).json({ error: "Error connecting to PLC" });
+    });
 });
 
-
-const port = process.env.PORT || 8000 ;
-app.listen(port, ()=> console.log(`Listening at port ${port}`));
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Listening at port ${port}`));
